96a61ac399c95dbc9cda63942ca9d269eef81412
arialdyscv <arialdyscv@gmail.com>
2022-06-11 10:45:27 -0400

    removed dummy data

--- src/app.js
+++ src/app.js
@@ -13,21 +13,6 @@ import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: "Water bill", amount: 150 }));
-store.dispatch(addExpense({ description: "gas bill", amount: 5000 }));
-store.dispatch(
-  addExpense({ description: "rent bill", amount: 950, createAt: 1000 })
-);
-store.dispatch(setTextFilter("water"));
-
-setTimeout(() => {
-  store.dispatch(setTextFilter("bill"));
-}, 3000);
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
