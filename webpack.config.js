70ae95a2d02ba8c113869c18cd1c18e1604d727b
arialdyscv <arialdyscv@gmail.com>
2022-06-16 13:30:55 -0400

    deleted package-lock.json to use yarn.lock

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,60 @@
 // entry -> output
-
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path:path.join(__dirname, '\public'),
-        filename: 'bundle.js'
-    },
-    // loader
-    module: {
-        rules:[{
-                loader: 'babel-loader',//loader to use jsx as js
-                test: /\.js$/, //regular expresion
-                exclude: /node_modules/
-            }, {
-                test: /\.s?css$/,
-                use: ['style-loader','css-loader', 'sass-loader']//adding loader for css and styles into the babel loader       
-            }]
-    },
-    devtool: 'inline-cheap-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, '\public'),
-        historyApiFallback: true //telling we're using clientside
+
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+    return {
+        entry: "./src/app.js",
+        output: {
+        path: path.join(__dirname, "public"),
+        filename: "bundle.js",
+      },
+      module: {
+        rules: [
+          {
+            test: /\.js$/, //regular expresion
+            exclude: /node_modules/,
+            use: { loader: "babel-loader"}, //loader to use jsx as js
+          },
+          {
+            test: /\.s?css$/,
+            use: [
+              { loader: MiniCssExtractPlugin.loader,
+                options: {
+                  publicPath: (resourcePath, context) => {
+                    return (
+                      path.relative(path.dirname(resourcePath), context) + '/'
+                    );
+                  },
+                },  
+              },
+              { loader: "css-loader", options: { sourceMap: true } },
+              { loader: "sass-loader", options: { sourceMap: true } },
+              
+            ], //adding loader for css and styles into the babel loader  
+          },
+        ],
+      },
+      plugins: [
+        new MiniCssExtractPlugin({
+          filename: 'styles.css',
+          chunkFilename: 'app.css'
+        })
+      ],
+      mode:"development",
+      devtool: isProduction? "source-map" : "inline-cheap-module-source-map",
+      resolve: {
+        extensions: ['.js', '.jsx', '.scss']
+      },
+      devServer: {
+        static:{
+          directory: path.join(__dirname, "public") },
+          compress: true,
+          port: 9000,
+        historyApiFallback: true, //telling we're using clientside
+      },
     }
+ 
 };
-
-
-
-
