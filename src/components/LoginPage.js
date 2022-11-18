3f440cd4f7a952c8a5e23030c75b8125af9a9730
arialdyscv <arialdyscv@gmail.com>
2022-11-18 12:07:51 -0400

    added login background

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { startLogin } from '../actions/auth';
-
+import React, { Fragment } from "react";
+import { connect } from "react-redux";
+import { startLogin } from "../actions/auth";
 
 export const LoginPage = ({ startLogin }) => (
-    <div className='box-layout'>
-        <div className='box-layout__box'>
-            <h1 className='box-layout__title'>Expensify App</h1>
-            <h3>Time to record your expenses</h3>
-            <button className='button button--login' onClick={startLogin}>Login with Google</button>
-        </div>
-      
+  <div className="box-layout">
+    <div className="login-bg"></div>
+    <div className="login-bg_bg2"></div>
+    <div className="login-bg_bg3"></div>
+    <div className="login-content">
+      <div className="box-layout__box">
+        <h1 className="box-layout__title">Expensify App</h1>
+        <h3>Time to record your expenses</h3>
+        <button className="button button--login" onClick={startLogin}>
+          Login with Google
+        </button>
+      </div>
     </div>
+  </div>
 );
 
 const mapDispatchToProps = (dispatch) => ({
-    startLogin: () => dispatch(startLogin())
+  startLogin: () => dispatch(startLogin()),
 });
 export default connect(undefined, mapDispatchToProps)(LoginPage);
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,37 +1,40 @@
-import React from 'react';
-import { Router, Route, Switch } from 'react-router-dom';/*allows to create multiple pages using route. 
+import React from "react";
+import {
+  Router,
+  Route,
+  Switch,
+} from "react-router-dom"; /*allows to create multiple pages using route. 
 dom is for websites and native is for phone-apps*/
-import { createBrowserHistory } from 'history';
-import AddExpensePage from '../components/AddExpensePage';
-import EditExpensePage from '../components/EditExpensePage';
-import ExpenseDashboardPage from '../components/DashboardPage';
-import LoginPage from '../components/LoginPage';
-import NotFoundPage from '../components/NotFoundPage';
-import PrivateRoute from '../router/PrivateRoute';
-import PublicRoute from '../router/PublicRoute';
-
+import { createBrowserHistory } from "history";
+import AddExpensePage from "../components/AddExpensePage";
+import EditExpensePage from "../components/EditExpensePage";
+import ExpenseDashboardPage from "../components/DashboardPage";
+import LoginPage from "../components/LoginPage";
+import NotFoundPage from "../components/NotFoundPage";
+import PrivateRoute from "../router/PrivateRoute";
+import PublicRoute from "../router/PublicRoute";
 
 export const history = createBrowserHistory();
 /*<BrowserRouter/> already comes with it 
 but having it separated can be manipulated*/
 
 const AppRouter = () => (
-    //Switch looks into each url from top to bottom until finds a match. Route without path will always match
-    <Router history={history}>
-        <div>
-            <Switch>
-                <PublicRoute path='/' component={LoginPage} exact={true} />
-                <PrivateRoute path='/dashboard' component={ExpenseDashboardPage} />
-                <PrivateRoute path='/create' component={AddExpensePage} />
-                <PrivateRoute path='/edit/:id' component={EditExpensePage} />
-                <Route component={NotFoundPage} />
-            </Switch>
-        </div>
-    </Router>
+  //Switch looks into each url from top to bottom until finds a match. Route without path will always match
+  <Router history={history}>
+    <div>
+      <Switch>
+        <PublicRoute path="/" component={LoginPage} exact={true} />
+        <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+        <PrivateRoute path="/create" component={AddExpensePage} />
+        <PrivateRoute path="/edit/:id" component={EditExpensePage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </Router>
 );
 // Route takes in 2 props the path and a component. So it means that if it matches this path render this component
 // Browser Router expects a single element inside so we use a div to have multiple routes
 // exact=true means that  it will only show this component if it exactly matches this path. because this component can show up on other pages since it has the same starting path.
-//Swith do: it will go through all the routes and stop on the path that matches. If no path matches it will continue to the bottom the 404pages doesnt have a path but it means it always matches with any path.
+//Switch do: it will go through all the routes and stop on the path that matches. If no path matches it will continue to the bottom the 404pages doesn't have a path but it means it always matches with any path.
 //path="/edit/:id" whatever comes after edit it will go.
 export default AppRouter;
